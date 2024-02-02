2ae869cfafa52705945d7227272aa03310cd3f21
Jeremy Groce <JJGroce95@gmail.com>
2024-02-02 10:47:34 -0600

    created backend

---
+++ src/backend/routes/data.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    // res.json({ id: 1, title: 'running', description: 'at least 1 mile'});
+});
+
+module.exports = router;
