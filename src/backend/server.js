81c6a236da594a20a41fd4a77331f778547eaf02
Jeremy Groce <JJGroce95@gmail.com>
2024-02-06 09:42:43 -0600

    WORKING SERVER HAHAHAHAHA

--- src/backend/server.js
+++ src/backend/server.js
@@ -2,10 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 3001;  //local host
-const dataRouter = require('./routes/data');
 
-app.use('/api/data', dataRouter);
 app.use(cors());
+
+// Sample data
+let data = ["sample data"];
+
+// GET request
+app.get('/',(req,res) => {
+    res.json(data);
+})
 
 app.listen(port, () => {
     console.log("server is now running");
--- src/components/hobbyList.js
+++ src/components/hobbyList.js
@@ -4,18 +4,24 @@ import '../styling/hobbyList.css';
 
 // fetch and display modules on render
 function HobbyList({reGET, resetTrigger}) {
-  // const backendURL = 'http://localhost:3001';
+  const backendURL = 'http://localhost:3001';
 
   // NOTE: Need to do GET based on date
 
+  let testString = '';
+
   // Initial GET on render
   useEffect(()=>{
-  // fetch(`${backendURL}/api/hobbies`)
-  //   .then(response => response.json())
-  //   .then(data => {})
-  //   .catch(error => {
-  //     console.error('Error fetching data:', error);
-  //   });
+  fetch(`${backendURL}/`)
+    .then(response => response.json())
+    .then(data => {
+        testString = JSON.stringify(data);
+        console.log('GET: ', testString);
+
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
   }, []);
 
   // conditionally call another GET request on event trigger
